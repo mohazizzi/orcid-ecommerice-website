3dab410144f37543e306cbbd559752afcb0d1ff9
Mohamadreza Azizi <mohazizzi.mr@gmail.com>
2022-10-12 01:10:55 +0330

    change favicon and title

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css';
+import Home from './pages/home/Home';
+
+import ErrorPage from './pages/404/error-page';
+
+import { createBrowserRouter, RouterProvider, Route} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <ErrorPage />,
+  },
+  // {
+  //   path: "contacts/:contactId",
+  //   element: <Contact />,
+  // },
+]);
+
+function App() {
+  return (
+    <RouterProvider router={router} />
+  );
+}
+
+export default App;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+      <div>
+        سلام
+      </div>
+    );
+  }
+  
+  export default Home;
+  
